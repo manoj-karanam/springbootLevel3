8cbfed6cd148e70451de31976b254b81de4116ab
Sai Manoj Karanam <151407816+manojkaranamm-stack@users.noreply.github.com>
2025-01-14 14:08:13 -0500

    6 - @ConfigurationProperties

--- spring-boot-config/src/main/java/io/javabrains/spring_boot_config/GreetingController.java
+++ spring-boot-config/src/main/java/io/javabrains/spring_boot_config/GreetingController.java
@@ -1,6 +1,7 @@
 package io.javabrains.spring_boot_config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,12 +21,15 @@ public class GreetingController {
     @Value("${my.list.values}")
     private List<String> listValues;
 
-    @Value("#{${dbValues}}")
-    private Map<String,String> dbValues;
+    @Autowired
+    private dbSettings dbSettings;
+
+//    @Value("#{${dbValues}}")
+//    private Map<String,String> dbValues;
 
     @GetMapping("/greeting")
     public String greeeting(){
-        return greetingMessage + staticMessage + listValues + dbValues.get("password");
+        return dbSettings.getConnection() + dbSettings.getHost() + dbSettings.getPort();
 
     }
 }
---
+++ spring-boot-config/src/main/java/io/javabrains/spring_boot_config/dbSettings.java
@@ -0,0 +1,38 @@
+package io.javabrains.spring_boot_config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+@Configuration
+@ConfigurationProperties("db")
+public class dbSettings {
+    private String connection;
+    private String host;
+    private String port;
+
+    public String getConnection() {
+        return connection;
+    }
+
+    public void setConnection(String connection) {
+        this.connection = connection;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+}
